b20889e142b6aed6686865de4df5d05f3b3337db
victorrr <149414843+victorrr385@users.noreply.github.com>
2024-07-18 00:57:03 +0100

    Create picture.py

---
+++ picture.py
@@ -0,0 +1,15 @@
+import cv2
+
+face_haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+image = cv2.imread('Blog 1.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("Gray", gray)
+# cv2.waitKey()
+
+faces = face_haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h),(0,255,0), 5)
+
+cv2.imshow("Faces", image)
+cv2.waitKey()
