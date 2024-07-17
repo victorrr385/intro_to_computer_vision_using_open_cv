30ec4ee8a8eadb7e0c84d81a6ac39481bc1b9052
victorrr <149414843+victorrr385@users.noreply.github.com>
2024-07-18 00:58:47 +0100

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,15 +1,19 @@
- import cv2
+import cv2
 
-face_haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-image = cv2.imread('Blog 1.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("Gray", gray)
-# cv2.waitKey()
+capture = cv2.VideoCapture(0)
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+while True:
+    _, image = capture.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_haar_cascade.detectMultiScale(gray, 1.1, 4)
 
-faces = face_haar_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h),(0,255,0), 5)
+    cv2.imshow("Video", image)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
 
-cv2.imshow("Faces", image)
-cv2.waitKey()
+
+cv2.release()
